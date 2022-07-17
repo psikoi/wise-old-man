85e6f48b1c874c387aa2739b9860ddeaa5585e38
Psikoi <ruben.amendoeira@gmail.com>
2022-07-17 07:32:46 +0100

    wseoifgweiojf

--- server/scripts/run-tests.sh
+++ server/scripts/run-tests.sh
@@ -8,4 +8,4 @@ prisma migrate reset --force
 # Run jest on all unit and integration tests tests
 export NODE_ENV=test
 export TZ=UTC
-# jest __tests__/suites --detectOpenHandles --verbose --force-exit
+jest __tests__/suites --detectOpenHandles --verbose --force-exit
